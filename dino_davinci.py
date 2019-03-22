ea97b1fe12d67a7fdbdd8c120a4ca66d0bb9f4d1
theethaj <you@example.com>
2019-03-22 16:43:18 +0700

    first dino

--- dino_davinci.py
+++ dino_davinci.py
@@ -1,4 +1,5 @@
 import arcade
+from DinoModel import PlayerDino
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -10,8 +11,11 @@ class DinoDavWindow(arcade.Window):
 
         arcade.set_background_color(arcade.color.WHITE)
 
+        self.Chomp = DinoModel(filename="images/Player-Triceratops.gif", center_x=width//2, center_y=height//2)
+
     def on_draw(self):
         arcade.start_render()
+        self.Chomp.draw()
 
 
 def main():
@@ -19,6 +23,13 @@ def main():
     arcade.set_window(window)
     arcade.run()
 
+
+class DinoModel(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def draw(self):
+        super().draw()
 
 if __name__ == '__main__':
     main()
