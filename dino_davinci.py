7cbfe1209ec801870128caafb79f79f5ad22168f
theethaj <bank30827@gmail.com>
2019-04-26 11:59:19 +0700

    26/4

--- dino_davinci.py
+++ dino_davinci.py
@@ -7,12 +7,13 @@ from random import randint
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-PLAYER_DINOS = ["TYRANNOSAURUS REX", "TRICERATOPS", "SAICHANIA", "ISISAURUS"]
-PLAYER_LP = [2000, 1800, 1600, 2200]
-ADV_DINOS = ["DEINONYCHUS", "CRYOLOPHOSAURUS", "THERIZINOSAURUS", "MEGALOSAURUS"]
-ADV_LP = [4000, 5200, 4200, 4400]
-EXP_DINOS = ["GIGAS", "MAXIMUS", "ARMATUS", "BRONTIKENS"]
-EXP_LP = [5000, 4800, 4600, 5400]
+
+PLAYER_DINOS = ["ANKYLOSAURUS", "STYRACOSAURUS", "TYRANNOSAURUS REX", "AMPELOSAURUS"]
+PLAYER_LP = [1600, 1800, 2000, 2200]
+COM_DINOS = ["STEGOSAURUS", "NEDOCERATOPS", "AMPELOSAURUS", "BLACK T-REX",
+         "ARMATUS", "MAXIMUS", "GIGAS", "CRYOLOPHOSAURUS", "BRONTIKENS"]
+COM_LP = [1600, 1800, 2200, 2500, 3400, 3600, 3800, 4000, 4200]
+
 options = ["rock", "paper", "scissors"]
 
 
@@ -20,16 +21,14 @@ class Window(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.background = None
-        self.Chomp = DinoModel(filename="images/Terry.png", center_x=width//4, center_y=height//2)
-        self.Tank = DinoModel(filename="images/Gigas.png", center_x=width//1.3, center_y=height//2)
-        self.player_name = PLAYER_DINOS[0]
-        self.player_lp = PLAYER_LP[0]
-        self.com_name = EXP_DINOS[0]
-        self.com_lp = EXP_LP[0]
+        self.player_pic = DinoModel(filename="images/Terry.png", center_x=width//4, center_y=height//2)
+        self.com_pic = DinoModel(filename="images/Gigas.png", center_x=width//1.3, center_y=height//2)
+        self.player_name = PLAYER_DINOS[2]
+        self.player_lp = PLAYER_LP[2]
+        self.com_name = COM_DINOS[6]
+        self.com_lp = COM_LP[6]
         self.win = ''
-        self.rock_choice = ''
-        self.paper_choice = ''
-        self.scissors_choice = ''
+        self.clan = ''
 
     def set_up(self):
         self.background = arcade.load_texture("images/bg1.png")
@@ -38,19 +37,21 @@ class Window(arcade.Window):
         arcade.start_render()
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
                                       SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
-        self.Chomp.draw()
-        self.Tank.draw()
+        self.player_pic.draw()
+        self.com_pic.draw()
 
         arcade.draw_text(str(self.player_name), self.width - 770, self.height - 30, arcade.color.WHITE, 20)
         arcade.draw_text(str(self.player_lp), self.width - 770, self.height - 55, arcade.color.WHITE, 20)
+
         arcade.draw_text(str(self.com_name), self.width - 100, self.height - 30, arcade.color.BLACK, 20)
         arcade.draw_text(str(self.com_lp), self.width - 100, self.height - 55, arcade.color.BLACK, 20)
+
         arcade.draw_text("PRESS (R)ock, (P)aper and (S)cissors.", 100, 550, arcade.color.YELLOW, 10)
 
         if self.win == False:
-            arcade.draw_text("YOU LOSE !!", 100, 50, arcade.color.RED, 100)
+            arcade.draw_text("YOU LOSE !!", 300, 550, arcade.color.YELLOW, 40)
         elif self.win == True:
-            arcade.draw_text("YOU WIN !!", 100, 50, arcade.color.RED, 100)
+            arcade.draw_text("YOU WIN !!", 300, 550, arcade.color.YELLOW, 40)
 
     def on_key_press(self, key, key_modifies):
         if key == arcade.key.R:
